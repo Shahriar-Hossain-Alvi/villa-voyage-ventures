845178d7d8865f6e6cc1e15192b6f83065822fe9
Md. Shahriar Hossain Alvi <shahriarhossainalvi@gmail.com>
2024-04-08 00:53:24 +0600

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl'>Vite + React</h1>
+      <button className="btn btn-circle btn-primary">Click me</button>
+    </>
+  )
+}
+
+export default App
