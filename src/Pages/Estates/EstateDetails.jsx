b70be6eab55763f80c75815bd964dfde28228824
Md. Shahriar Hossain Alvi <shahriarhossainalvi@gmail.com>
2024-04-13 05:42:33 +0600

    estate details page responsive done

--- src/Pages/Estates/EstateDetails.jsx
+++ src/Pages/Estates/EstateDetails.jsx
@@ -28,29 +28,29 @@ const EstateDetails = () => {
             <Navbar></Navbar>
 
             <div className="mt-8">
-                <h3 className="text-5xl text-center font-bold font-playfairDisplay">{estate_title}</h3>
+                <h3 className="text-3xl md:text-5xl lg:text-5xl text-center font-bold font-playfairDisplay">{estate_title}</h3>
 
                 {/* images */}
-                <div className="grid grid-cols-3 mt-10 gap-6">
-                    <div className="col-span-2">
+                <div className="grid md:grid-cols-2 lg:grid-cols-3 mt-10 gap-6">
+                    <div className="lg:col-span-2">
                         <img className="w-full h-[450px]" src={image_link} alt="" />
                     </div>
                     <div className="flex flex-col gap-4">
-                        <img className="w-full h-[217px]" src={image_2} alt="" />
-                        <img className="w-full h-[217px]" src={image_3} alt="" />
+                        <img className=" md:w-full lg:w-full md:h-[217px] lg:h-[217px]" src={image_2} alt="" />
+                        <img className=" md:w-full lg:w-full md:h-[217px] lg:h-[217px]" src={image_3} alt="" />
                     </div>
                 </div>
 
 
                 {/* details */}
-                <div className="my-8">
+                <div className="my-8 px-2 md:px-4 lg:px-0">
                     {/* description top */}
-                    <div className="flex justify-between border-b-2 pb-6 border-dashed">
+                    <div className="flex flex-col items-center lg:flex-row lg:justify-between border-b-2 pb-6 border-dashed space-y-2 md:space-y-3 lg:space-y-0">
                         <div>
-                            <h2 className="font-semibold text-xl text-[#1cb2ff]">Property ID: {estate.id} </h2>
+                            <h2 className="font-semibold text-xl text-[#1cb2ff] mb-2">Property ID: {estate.id} </h2>
                             <p className="badge badge-lg text-white bg-[#1cb2ff]">{segment_name}</p>
                         </div>
-                        <div className="flex gap-6">
+                        <div className="flex gap-1 md:gap-3 lg:gap-6">
                             <button className="btn bg-[#1cb2ff] text-white hover:bg-secondaryColor  tooltip" data-tip="Share">
                                 <FaShareAlt />
                             </button>
@@ -67,8 +67,9 @@ const EstateDetails = () => {
                     </div>
 
 
-                    <div className="mt-10 grid grid-cols-3 gap-6">
-                        <div className="col-span-2">
+                    <div className="mt-10 grid lg:grid-cols-3 gap-6">
+                        {/* description details */}
+                        <div className="lg:col-span-2">
                             <h2 className="text-[#1cb2ff] font-bold pb-3 border-b mb-3">Description:</h2>
                             <p className="pt-4 leading-7">{detailed_description}</p>
                         </div>
